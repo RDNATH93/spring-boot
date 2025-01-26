782b3837b7daef955eecd037e840bf632dd402e1
Rahul Debnath <67674099+RDNATH93@users.noreply.github.com>
2025-01-26 10:07:21 +0000

    initial commit

---
+++ function_stored_procedures/src/main/java/com/example/function_stored_procedures/FunctionStoredProceduresApplication.java
@@ -0,0 +1,13 @@
+package com.example.function_stored_procedures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionStoredProceduresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionStoredProceduresApplication.class, args);
+	}
+
+}
---
+++ function_stored_procedures/src/test/java/com/example/function_stored_procedures/FunctionStoredProceduresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.function_stored_procedures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionStoredProceduresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
