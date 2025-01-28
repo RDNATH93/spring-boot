91e3a1568dbc33f741d6d6fe57292bb8ba66622e
Rahul Debnath <67674099+RDNATH93@users.noreply.github.com>
2025-01-28 07:00:53 +0000

    initial commit

---
+++ locking-demo/src/main/java/com/example/locking_demo/LockingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.locking_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockingDemoApplication.class, args);
+	}
+
+}
---
+++ locking-demo/src/test/java/com/example/locking_demo/LockingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locking_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
