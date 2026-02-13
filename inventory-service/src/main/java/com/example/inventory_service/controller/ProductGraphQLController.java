28e79818dc4540a93c2ca9c08b9f6a8842abb128
Rahul Debnath <67674099+RDNATH93@users.noreply.github.com>
2026-02-13 09:59:33 +0000

    crud using graphql

---
+++ inventory-service/src/main/java/com/example/inventory_service/controller/ProductGraphQLController.java
@@ -0,0 +1,43 @@
+package com.example.inventory_service.controller;
+
+import java.util.List;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.inventory_service.entity.Product;
+import com.example.inventory_service.service.ProductService;
+
+@Controller
+public class ProductGraphQLController {
+    
+    private final ProductService productService;
+
+    ProductGraphQLController(ProductService productService){
+        this.productService=productService;
+    }
+
+    @QueryMapping
+    List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+    
+    @QueryMapping
+    List<Product> getProductsByCategory(@Argument String category) {
+        return productService.getProductsByCategory(category);
+    }
+
+    @MutationMapping
+    Product updateStock(@Argument int id,@Argument int qty){
+        return productService.updateStock(id, qty);
+    }
+
+    @MutationMapping
+    Product receiveNewShipment(@Argument int id,@Argument int stock){
+        return productService.receiveNewShipment(id, stock);
+    }
+}
--- inventory-service/src/main/java/com/example/inventory_service/service/ProductService.java
+++ inventory-service/src/main/java/com/example/inventory_service/service/ProductService.java
@@ -9,19 +9,35 @@ import com.example.inventory_service.repository.ProductRepository;
 
 @Service
 public class ProductService {
-    
+
     private final ProductRepository productRepository;
 
-    ProductService(ProductRepository productRepository){
-        this.productRepository=productRepository;
+    ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
     }
 
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
-    public List<Product> getProductsByCategory(String category){
+    public List<Product> getProductsByCategory(String category) {
         return productRepository.findByCategory(category);
-       
+
+    }
+
+    public Product updateStock(int id, int quantity) {
+        Product existingProduct = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id " + id));
+
+        existingProduct.setStock(quantity);
+        return productRepository.save(existingProduct);        
+    }
+
+    public Product receiveNewShipment(int id, int quantity){
+                    Product existingProduct = productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found with id " + id));
+
+        existingProduct.setStock(existingProduct.getStock()+quantity);
+        return productRepository.save(existingProduct); 
     }
 }
