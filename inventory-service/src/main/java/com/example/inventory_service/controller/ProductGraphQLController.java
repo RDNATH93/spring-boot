af84e6d5d3eb63ab08973a4c99e7cb3d61a46682
Rahul Debnath <67674099+RDNATH93@users.noreply.github.com>
2026-02-13 11:08:08 +0000

    updated

--- inventory-service/src/main/java/com/example/inventory_service/controller/ProductGraphQLController.java
+++ inventory-service/src/main/java/com/example/inventory_service/controller/ProductGraphQLController.java
@@ -6,8 +6,6 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.inventory_service.entity.Product;
 import com.example.inventory_service.service.ProductService;
